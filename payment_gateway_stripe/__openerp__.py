b14077012b297735894c5ee6981d189c6c748be6
Sébastien BEAU <sebastien.beau@akretion.com>
2017-04-11 09:29:21 +0200

    [FIX] add missing dependency

--- payment_gateway_stripe/__openerp__.py
+++ payment_gateway_stripe/__openerp__.py
@@ -14,7 +14,7 @@
     "application": False,
     "installable": True,
     "external_dependencies": {
-        "python": [],
+        "python": ['stripe'],
         "bin": [],
     },
     "depends": [
